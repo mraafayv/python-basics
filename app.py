badef43a08dff5154717a78913861b008ced2167
Muhammad Raafay <raafayvohra.mrv@gmail.com>
2025-03-12 10:38:42 +0100

    add mathematical operations

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
 print("Hello World")
+
+
+x = int(4.7)   # x is now an integer 4
+y = float(4)   # y is now a float of 4.0
+
+
+print(type(x))
+print(type(y))
+
+
+a = 4 ** 2  #4 squared by 2 
+print(a);
+
+b = 4 - 2 * 6 / 2
+
+print(b);
